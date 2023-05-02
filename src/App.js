102ad82588f7ad6a764c08daa4160aaf93f88175
Minimage <seancampbell914.com>
2023-05-01 23:20:16 -0400

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      ></input>
+      <input
+        type="password"
+        name="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      ></input>
+
+      {email}
+      {password}
     </div>
   );
 }
---
+++ src/useForm.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+export const useForm = (initalValues) => {
+  const [values, setValues] = useState(initalValues);
+
+  return <></>;
+};
